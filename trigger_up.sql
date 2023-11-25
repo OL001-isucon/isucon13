e2cc460db9a74ab26723e70e123e31aad45b7e8c
Nokoyohei <kono@openlogi.com>
2023-11-25 14:55:24 +0900

    fix trigger

--- trigger_up.sql
+++ trigger_up.sql
@@ -1,39 +1,21 @@
--- update_user_stats
 DELIMITER //
 CREATE TRIGGER update_user_stats
 AFTER INSERT ON reactions
 FOR EACH ROW
 BEGIN
-    -- Update reaction_count in user_stats table
-    UPDATE user_stats
-    SET reaction_count = reaction_count + 1
-    WHERE user_id = NEW.user_id;
-
-    -- If no record exists for the user_id, insert a new one
-    IF ROW_COUNT() = 0 THEN
-        INSERT INTO user_stats (user_id, reaction_count, tip_amount)
-        VALUES (NEW.user_id, 1, 0);
-    END IF;
+  INSERT INTO user_stats (user_id, reaction_count, tip_amount)
+  VALUES (NEW.user_id, 1, 0) ON DUPLICATE KEY UPDATE reaction_count = reaction_count + 1;
 END;
 //
 DELIMITER ;
 
--- update_user_stats_livecomments
 DELIMITER //
 CREATE TRIGGER update_user_stats_livecomments
 AFTER INSERT ON livecomments
 FOR EACH ROW
 BEGIN
-    -- Update tip_amount in user_stats table
-    UPDATE user_stats
-    SET tip_amount = tip_amount + NEW.tip
-    WHERE user_id = NEW.user_id;
-
-    -- If no record exists for the user_id, insert a new one
-    IF ROW_COUNT() = 0 THEN
-        INSERT INTO user_stats (user_id, reaction_count, tip_amount)
-        VALUES (NEW.user_id, 0, NEW.tip);
-    END IF;
+  INSERT INTO user_stats (user_id, reaction_count, tip_amount)
+  VALUES (NEW.user_id, 0, NEW.tip) ON DUPLICATE KEY UPDATE tip_amount = tip_amount + NEW.tip;
 END;
 //
 DELIMITER ;
